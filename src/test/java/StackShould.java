2ebf7a2cc2ab642252abe16ae96a3c5178cf8090
Carlos Cano <ccano@squareup.com>
2022-03-04 17:17:27 +0100

    refactor stack kata

--- src/test/java/StackShould.java
+++ src/test/java/StackShould.java
@@ -1,32 +1,31 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class StackShould {
 
     private final Stack stack = new Stack();
 
-
     @Test
-    void throw_exception_if_popped_when_empty() {
-        Assertions.assertThrows(IndexOutOfBoundsException.class, stack::pop);
+    void throw_an_exception_if_stack_is_empty() {
+        assertThrows(EmptyStackException.class,
+                stack::pop);
     }
 
     @Test
-    void add_object_when_push() {
-        stack.push(new Object());
-        stack.push(new Object());
+    void pop_the_last_element_pushed() {
+        Object elementOne = "element one";
+        Object elementTwo = "element two";
 
-        Assertions.assertEquals(2, stack.getStack().size());
+        stack.push(elementOne);
+        stack.push(elementTwo);
+
+        assertThat(stack.pop()).isEqualTo(elementTwo);
+        assertThat(stack.pop()).isEqualTo(elementOne);
     }
 
-    @Test
-    void return_and_remove_object_when_pop() {
-        var object = new Object();
-        stack.push(object);
-
-        Assertions.assertEquals(1, stack.getStack().size());
-        Assertions.assertEquals(object, stack.pop());
-        Assertions.assertEquals(0, stack.getStack().size());
-    }
 }
